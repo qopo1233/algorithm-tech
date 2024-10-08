ef9257537e0e6c65bf1e6ebe4736f7eff70b1c72
yangchuanhua <yangchuan_hua@163.com>
2024-10-08 15:52:24 +0800

    队列实现栈

---
+++ src/com/magnus/algorithm/stackandqueue/MyStack.java
@@ -0,0 +1,62 @@
+package com.magnus.algorithm.stackandqueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @ClassName MyStack
+ * @Description 225. 用队列实现栈
+ * 简单
+ * 相关标签
+ * 相关企业
+ * 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+ * <p>
+ * 实现 MyStack 类：
+ * <p>
+ * void push(int x) 将元素 x 压入栈顶。
+ * int pop() 移除并返回栈顶元素。
+ * int top() 返回栈顶元素。
+ * boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+ * <p>
+ * <p>
+ * 注意：
+ * <p>
+ * 你只能使用队列的标准操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+ * 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+ * @Author yangchuanhua1
+ * @Date 2024/10/2 21:53
+ */
+public class MyStack {
+
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        queue1.offer(x);
+        while (!queue2.isEmpty()) {
+            queue1.offer(queue2.poll());
+        }
+
+    }
+
+    public int pop() {
+        return queue1.poll();
+    }
+
+    public int top() {
+        return queue1.peek();
+    }
+
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
