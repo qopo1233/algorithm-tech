e867dceedc3b0b2fa3794c05885b0d0c50d0307d
yangchuanhua <yangchuanhuacc@gmail.com>
2025-05-27 18:47:54 +0800

    增加反转列表的递归实现

--- src/com/magnus/algorithm/linkedlist/ReverseList.java
+++ src/com/magnus/algorithm/linkedlist/ReverseList.java
@@ -4,7 +4,7 @@ import com.magnus.algorithm.ListNode;
 
 /**
  * @ClassName ReverseList
- * @Description
+ * @Description 链表反转
  * @Author yangchuanhua1
  * @Date 2024/8/7 16:18
  */
@@ -14,20 +14,42 @@ public class ReverseList {
 
     }
 
+    /**
+     * 迭代反转
+     * @param head
+     * @return
+     */
     public static ListNode reverseList(ListNode head) {
 
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        ListNode pre = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = pre;
+            pre = current;
+            current = next;
         }
-        return prev;
+        return pre;
     }
 
-    public ListNode reverseList1(ListNode head) {
-        return null;
+    /**
+     * 递归实现
+     * @param head
+     * @return
+     */
+    public ListNode reverseListRecursive(ListNode head) {
+        // 基本情况：空链表或只有一个节点
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 递归反转剩余链表
+        ListNode newHead = reverseListRecursive(head.next);
+
+        // 改变指针方向
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
